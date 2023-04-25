3d79ee22a0feecc26399000b58d7ca06c6e7c8ae
anil-techasoft <anil.kumar@techasoft.com>
2023-04-25 08:12:36 +0530

    first commit

---
+++ src/get_data.py
@@ -0,0 +1,20 @@
+import yaml
+import argparse
+
+
+def get_data(config_file):
+    config = read_params(config_file)
+    return config
+
+
+def read_params(config_file):
+    with open(config_file) as conf:
+        config = yaml.safe_load(conf)
+        return config
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default='params.yaml')
+    passed_args = args.parse_args()
+    a = get_data(config_file=passed_args.config)
